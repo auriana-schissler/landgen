dc3aaff382aedfc6c2bb5614d62c5e897df1bc5a
Auriana Schissler <als@auri.codes>
2024-09-08 18:54:56 -0400

    Fixed issues preventing proper PPM file writing

--- src/file/ppm.rs
+++ src/file/ppm.rs
@@ -5,36 +5,31 @@ use std::io::Write;
 use std::sync::Arc;
 
 pub(super) fn write_to<W: Write>(state: Arc<RenderState>, writer: &mut W) -> Result<(), io::Error> {
-    writeln!(writer, "P6")?;
-    writeln!(writer, "#fractal planet image")?;
-    writeln!(writer, "# Command line:")?;
-    writeln!(writer, "# {}", get_commandline_footer())?;
-    writeln!(
-        writer,
-        "{} {} 255",
-        state.options.slicing.width, state.options.slicing.height
-    )?;
+    writeln!(writer, "P6\n{}\n{}\n255",
+             state.options.slicing.width as u32, state.options.slicing.height as u32)?;
 
     let shading = state.shading.read().unwrap();
     
     for (vi, v) in state.canvas.read().unwrap().iter().enumerate() {
-        for (hi, h) in v.iter().rev().enumerate() {
-            for (i, color_index) in h.iter().enumerate().map(|x| (x.0, *x.1 as usize)) {
+        for (hi, h) in v.iter().enumerate() {
+            for (wi, color_index) in h.iter().enumerate().map(|x| (x.0, *x.1 as usize)) {
                 let color = &state.options.color_table[color_index];
 
                 if state.options.shading_level > 0 {
-                    let shade = shading[vi][hi][i] as u32;
+                    let shade = shading[vi][hi][wi] as u32;
                     writer.write_all(&[
-                        ((shade * color.blue as u32) / 150).min(255) as u8,
-                        ((shade * color.green as u32) / 150).min(255) as u8,
                         ((shade * color.red as u32) / 150).min(255) as u8,
+                        ((shade * color.green as u32) / 150).min(255) as u8,
+                        ((shade * color.blue as u32) / 150).min(255) as u8,
                     ])?;
                 } else {
-                    writer.write_all(&[color.blue, color.green, color.red])?;
+                    writer.write_all(&[color.red, color.green, color.blue])?;
                 }
             }
         }
     }
+    writeln!(writer, "#fractal planet image")?;
+    writeln!(writer, "#{}", get_commandline_footer())?;
 
     writer.flush()?;
     Ok(())
